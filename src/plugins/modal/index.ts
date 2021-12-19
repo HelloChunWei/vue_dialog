2d9d141e1593553b60b2407e7761d53684c2be5c
Neil Guo <kissme3131@gmail.com>
2021-12-19 18:06:11 +0800

    reactive props

--- src/plugins/modal/index.ts
+++ src/plugins/modal/index.ts
@@ -1,4 +1,4 @@
-import { createApp } from 'vue'
+import { createApp, reactive, toRefs, h } from 'vue'
 import Modal from './modal.vue'
 
 type componentNameType = 'confirmModal' | 'inputModal'
@@ -7,16 +7,35 @@ export const useModal = () => {
   const openModal = ({ component, myProps }: { component: componentNameType, myProps: any }) => {
     const container = document.createElement('div')
     const _closeModal = function () {
+      data.isShow = false
       container.parentNode!.removeChild(container)
-    };
-    const localProp = Object.assign({}, {
+    }
+
+    const data = reactive({
       isShow: true,
       component,
       myProps,
       close: _closeModal
     })
 
-    const vnode = createApp(Modal, localProp)
+    const mockModal = {
+      name: 'mockModal',
+      setup () {
+        return {
+          ...toRefs(data)
+        }
+      },
+      render () {
+        return h(Modal, {
+          isShow: data.isShow,
+          component: data.component,
+          myProps: data.myProps,
+          close: data.close
+        })
+      }
+    }
+
+    const vnode = createApp(mockModal)
     document.body.appendChild(container)
     vnode.mount(container)
   }
