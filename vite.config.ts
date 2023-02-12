b12cedb73fb3749729e46bc62274a7d07e790b78
Neil Guo <kissme3131@gmail.com>
2023-02-12 14:59:23 +0800

    support tsx

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueTypeImports from 'vite-plugin-vue-type-imports'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    VueTypeImports()
+    VueTypeImports(),
+    vueJsx()
   ]
 })
