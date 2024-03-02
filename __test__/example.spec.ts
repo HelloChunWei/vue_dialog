252373e1cff702ba6ab0a358d85f2c3df84c3bcb
Neil Kuo <kissme3131@gmail.com>
2024-03-02 16:37:56 +0800

    delete playwright and use pnpm

--- __test__/example.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('homepage has title', async ({ page }) => {
-  await page.goto('http://localhost:3000/index.html');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Vite App/);
-});
