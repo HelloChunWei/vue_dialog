79f1d59dbbefbf23097e1cb98005ffa74e19cce0
Neil Kuo <kissme3131@gmail.com>
2024-03-02 17:26:49 +0800

    fix file name

--- src/plugins/dialog/index.ts
+++ src/plugins/dialog/index.ts
@@ -1,15 +1,55 @@
-import { ComponentOptions, VNodeProps, createApp, reactive, toRefs, h, provide } from 'vue'
-import { CLOSE_DIALOG } from './provideInject'
+import {
+  ComponentOptions,
+  VNodeProps,
+  createApp,
+  reactive,
+  toRefs,
+  h,
+  provide,
+  DefineComponent,
+  ComputedOptions,
+  MethodOptions,
+  ComponentOptionsMixin,
+  EmitsOptions,
+} from 'vue'
 import Modal from './Modal.vue'
+import { CLOSE_DIALOG } from './provideInject'
+
+
+
+type FilterProps<T> = {
+  [K in keyof T]: T[K] extends { type: StringConstructor; required: true } ? { type: string } : never;
+}
+
+type TransformProps<Props> = {
+  [P in keyof FilterProps<Props>]: FilterProps<Props>[P]['type']
+}
+
 
 export const useDialog = () => {
 
   // 如果 component 沒有 props 這樣就可以 openDialog(component)
   // 後面就不用帶成這樣 openDialog(component, {})
-  function openDialog(component: ComponentOptions<{}>): void
+  function openDialog(component: DefineComponent<{}>): void
 
   // component 是有定義props 的話是走這個
-  function openDialog<P>(component: ComponentOptions<P>, props: P): void;
+  function openDialog<
+    P,
+    A,
+    B,
+    C extends ComputedOptions,
+    D extends MethodOptions,
+    E extends ComponentOptionsMixin,
+    F extends ComponentOptionsMixin,
+    G extends EmitsOptions,
+    H extends string
+  >(component: DefineComponent<
+    P,
+    A,
+    B,
+    C,
+    E
+    >, props: test<P>): void;
 
   // 利用 typescript function overload 去實作
   function openDialog( component: any, props?: any){
